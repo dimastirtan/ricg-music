60834c912597e1a013830d456a7963b1c0087b78
Ferginho77 <fergiefakhirusalam@gmail.com>
2024-12-03 08:55:16 +0700

    mix

--- src/App.js
+++ src/App.js
@@ -68,14 +68,40 @@ function Card() {
 function RowCards() {
   return (
     <div className="row mt-3">
-      <div className="col-md-6">
+      <div className="col-md-4">
         <Card />
       </div>
-      <div className="col-md-6">
+      <div className="col-md-4">
         <Card />
+      </div>
+      <div className="col-md-4">
+        <ExclusiveCard />
       </div>
     </div>
   );
+}
+
+function LongCard(){
+  return ( 
+    <div className="row">
+      <div className="col-md-8">
+        <Card />
+    </div>
+    </div>
+  );
+}
+
+function ExclusiveCard() {
+  return (
+    <div className="card-header bg-danger mt-3">
+    <div className="card-header bg-danger">
+      <b className="ms-3" >EXCLUSIVE</b>
+    </div>
+    <div className="card text-bg-dark">
+    <img src={logo} alt="Small Slide 1" className="my-5 p-5 img-fluid" /> 
+    </div>
+  </div>
+  )
 }
 
 function Container() {
@@ -85,23 +111,24 @@ function Container() {
         <div className="col-md-8">
           <Carousel />
         </div>
-        <div className="ms-3 mt-3" style={{ width: "30%" }}>
+        <div className="col-md-4 mt-3">
           <SmallCarousel />
         </div>
       </div>
-      <RowCards /> {/* Added row of 2 cards */}
+      <RowCards /> 
       <div className="mt-3">
-        <Card /> {/* Added 1 card below small carousel */}
+        <LongCard /> 
       </div>
+      <Footer />
     </div>
   );
 }
 
 function SmallCarousel() {
   return (
-    <div className="card-header">
-      <div className="card-header">
-        <h2>EXCLUSIVE</h2>
+    <div className="card-header bg-danger">
+      <div className="card-title bg-danger">
+        <b className="ms-3">EXCLUSIVE</b>
       </div>
       <div id="smallCarousel" className="carousel slide bg-secondary-subtle" data-bs-ride="carousel">
         <div className="carousel-inner">
@@ -171,6 +198,112 @@ function Carousel() {
         <span className="visually-hidden">Next</span>
       </button>
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-light p-4 mt-3">
+      <div className="container">
+        <div className="row">
+          {/* About Section */}
+          <div className="col-md-4 text-center text-md-start">
+            <h5>About Us</h5>
+            <p>
+              Rich Music adalah platform berita musik terdepan yang menyediakan
+              informasi terbaru dan ulasan musik favorit Anda.
+            </p>
+          </div>
+
+          {/* Navigation Links */}
+          <div className="col-md-4 text-center">
+            <h5>Quick Links</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#home" className="text-light text-decoration-none">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#news" className="text-light text-decoration-none">
+                  News
+                </a>
+              </li>
+              <li>
+                <a href="#contact" className="text-light text-decoration-none">
+                  Contact Us
+                </a>
+              </li>
+              <li>
+                <a href="#about" className="text-light text-decoration-none">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Social Media Links */}
+          <div className="col-md-4 text-center text-md-end">
+            <h5>Follow Us</h5>
+            <div>
+              <a
+                href="https://www.facebook.com/richmusiconline"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-light mx-2"
+              >
+                <i className="bi bi-facebook"></i>
+              </a>
+              <a
+                href="https://x.com/richmusiconline"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-light mx-2"
+              >
+                <i className="bi bi-twitter"></i>
+              </a>
+              <a
+                href="https://www.instagram.com/richmusiconline/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-light mx-2"
+              >
+                <i className="bi bi-instagram"></i>
+              </a>
+              <a
+                href="https://www.youtube.com/channel/UCBC_ymsb8NEIUsShzxuuixw"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-light mx-2"
+              >
+                <i className="bi bi-youtube"></i>
+              </a>
+              <a
+                href="https://www.tiktok.com/@richmusiconline"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-light mx-2"
+              >
+                <i className="bi bi-tiktok"></i>
+              </a>
+              <a
+                href="https://www.threads.net/@richmusiconline"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-light mx-2"
+              >
+                <i className="bi bi-threads"></i>
+              </a>
+
+            </div>
+          </div>
+        </div>
+        <hr className="border-light" />
+        <div className="text-center">
+          <p className="m-0">Copyright &copy; 2023 Rich Music. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
   );
 }
 
